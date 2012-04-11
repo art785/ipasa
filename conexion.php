9f976cc2a7088931f57b077336a0388833cf6a8e
root <root@art-laptop.(none)>
2012-04-11 17:59:10 -0500

    la conexion persiste,hay que eliminarla hahahahha

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@
   public function MySQL(){ 
     if(!isset($this->conexion)){
 
-      $this->conexion = (mysql_connect("localhost","root","stratus"))
+      $this->conexion = (mysql_connect("localhost","root","corvette"))
 
         or die(mysql_error());
       mysql_select_db("ipasa",$this->conexion) or die(mysql_error());
