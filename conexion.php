0c82da433ec032c3b7dacf3f35138aa16e15169f
root <root@art-laptop.(none)>
2012-04-17 11:23:25 -0500

    cont

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@
   public function MySQL(){ 
     if(!isset($this->conexion)){
 
-      $this->conexion = (mysql_connect("localhost","root","stratus"))
+      $this->conexion = (mysql_connect("localhost","root","corvette"))
       
 
         or die(mysql_error());
