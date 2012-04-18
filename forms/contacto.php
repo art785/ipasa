0d2910014faf2f84bbed922ba4e25fdee1d71654
root <root@art-laptop.(none)>
2012-04-18 11:19:07 -0500

    cambios

--- forms/contacto.php
+++ forms/contacto.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <html>
     <head>
         <title>Contacto</title>
@@ -8,12 +9,12 @@
     </head>
         <body>
 		<form  id="contacto" method='POST' action=''>
-		<input id="fila" class="nombre" type="text" required="" autocomplete="off" placeholder="Nombre" value="Nombre" maxlength="60" title="Nombre" name="nombre" style="color: rgb(180, 177, 175);">
-		<input id="fila" class="asunto" type="text" autocomplete="off" placeholder="Asunto" value="asunto" maxlength="20" title="Asunto" name="asunto" style="color: rgb(180, 177, 175);">
-		<input id="fila" class="email" type="email" required="" autocomplete="off" placeholder="Correo electronico" value="Correo Electronico" maxlength="40" title="Correo electronico" name="email">
-		<textarea id="fila" class="mensaje" required="" placeholder="Escriba su Mensaje" maxlength="1000" rows="10" cols="49" title="Mensaje" name="mensaje" style="color: rgb(180, 177, 175);"></textarea>
+		 <div id="fila"><label>Nombre:</label> <span class="input_area"><input id="nombre" class="nombre" type="text" required="" autocomplete="off" placeholder="Nombre" value="Nombre" maxlength="60" title="Nombre" name="nombre" style="color: rgb(180, 177, 175);"></span></div>
+		 <div id="fila"><label>Asunto:</label> <span class="input_area"><input id="asunto" class="asunto" type="text" autocomplete="off" placeholder="Asunto" value="asunto" maxlength="20" title="Asunto" name="asunto" style="color: rgb(180, 177, 175);"></span></div>
+		 <div id="fila"><label>Email:</label> <span class="input_area"><input id="email" class="email" type="email" required="" autocomplete="off" placeholder="Correo electronico" value="Correo Electronico" maxlength="40" title="Correo electronico" name="email" style="color: rgb(180, 177, 175);"></span></div>
+		<br><label>Nombre:</label><br><textarea id="mensaje" class="mensaje" required="" placeholder="Escriba su Mensaje" maxlength="1000" rows="8" cols="49" title="Mensaje" name="mensaje" style="color: rgb(180, 177, 175);"></textarea>
 		
-			<div><input type='submit' value='Envia Mensaje' id='boton' name='boton'></div>
+			 <div id="fila2"> <span class="sub"><input type="image" src="../fondos_pagina/boton-enviar-comentario.png" value='' class='boton'> </span></div>
 		</form>
 		<div id="respuesta"></div>
 	</body>
