ad2c0d3c7022c45c91f6be07bb92033513fb2d1c
root <root@art-laptop.(none)>
2012-04-26 12:57:43 -0500

    camshu

---
+++ lib/doctrine/Sql_Slide_Banner.php
@@ -0,0 +1,26 @@
+<?php
+include "lib/entidad/Banner.php";
+include_once 'conexion.php';
+ class Sql_Slide_Banner 
+ {
+
+        public function rutas_slide()
+        {
+            $mysql = new MySQL();
+            $consulta = "SELECT ruta FROM Banner WHERE activo = 1";
+            $resultado=$mysql->consulta($consulta);
+            $ArrSlide = array();
+            $i=0;
+                         
+                                        while($objeto = $mysql->fetch_object($resultado))
+                                        {
+                                            
+                                        $ArrSlide[$i]=new Banner();
+                                        $ArrSlide[$i]->setRuta($objeto->ruta);
+                              
+                                        $i++;
+	                                 	} 
+                                       return $ArrSlide;
+        }
+ }
+?>
