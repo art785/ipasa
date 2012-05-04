f6847f1a3086682d8e4ef621cb405d25116075bc
Miguel Huerta <mikehf7@gmail.com>
2012-05-04 10:22:23 -0500

    cambios

--- lib/controller/SlideController.php
+++ lib/controller/SlideController.php
@@ -1,14 +1,31 @@
 <?php
-include 'lib/doctrine/Sql_Slide_Show.php';
-class SlideController extends Sql_Slide_Show{
+include 'doctrine/Sql_Slide_Show.php';
+include "entidad/Slide_Show.php";
+class SlideController {
+	
+	private $sql_slide;
+	
+	public function __construct()
+	{
+		$this->sql_slide = new Sql_Slide_Show();
+	}
+	
+	public function insertar_slide()
+	{
+		if(isset($_POST["slide_principal"]))
+		{
+			$ruta=$_POST["ruta"];
+			$descripcion=$_POST["descipcion"];
+			$slide= new Slide_Show();
+			$slide->setActivo(1);
+			$slide->setDescripcion($descripcion);
+			$slide->setRuta($ruta);
+			$this->sql_slide->agrega_slide($slide);
 
-    public function llena_slide()
-    {
-        $slides=$this->rutas_slide();
-        foreach ($slides as $slide)
-        {
-            print  '  <img style="width:700px; height:350px;" src="'.$slide->getRuta().'" alt="" title="'.$slide->getDescripcion().' " />';
-        }    
-    }
+		}
+		
+	}
 }
+
+
 ?>
